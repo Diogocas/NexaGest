9e67caf61b80629b67208cd204ddcf2fc8807add
Diogo Castro <diogocastrosilva2019@gmail.com>
2026-06-28 19:46:56 -0300

    NexaGest 9.3.2

--- PUBLICAR.bat
+++ PUBLICAR.bat
@@ -48,8 +48,8 @@ if not exist "%REPO%\.git" (
   exit /b 1
 )
 
-set /p VERSION=Versao para publicar (ex.: 9.3.1): 
-if "%VERSION%"=="" set "VERSION=9.3.1"
+set /p VERSION=Versao para publicar (ex.: 9.3.2): 
+if "%VERSION%"=="" set "VERSION=9.3.2"
 
 set /p MSG=Mensagem do commit (Enter para padrao): 
 if "%MSG%"=="" set "MSG=NexaGest %VERSION%"
